11b697a7f38a274e3da743283c8ae2e8cd4bf3b2
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2024-01-30 16:42:22 +0100

    Update

--- Assets/Scripts/MovableUI.cs
+++ Assets/Scripts/MovableUI.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using NaughtyAttributes;
 
-public class MovableUI : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler
+public class MovableUI : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
 {
     [Header("- SETUP -")]
     [SerializeField] private bool _zoomable;
@@ -46,11 +46,6 @@ public class MovableUI : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndD
         }
     }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        _pos = transform.position - Camera.main.ScreenToWorldPoint(eventData.position);
-        _image.raycastTarget = false;
-    }
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -61,4 +56,34 @@ public class MovableUI : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndD
     {
         _image.raycastTarget = true;
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _pos = transform.position - Camera.main.ScreenToWorldPoint(eventData.position);
+        _image.raycastTarget = false;
+    }
+
+    [Button]
+    void SimulateZoom()
+    {
+        UnityEngine.Touch touch0 = new Touch();
+        touch0.position = new Vector2(1000, 1000);
+        touch0.deltaPosition = new Vector2(800, 800);
+        UnityEngine.Touch touch1 = new Touch();
+        touch1.position = new Vector2(500, 500);
+        touch1.deltaPosition = new Vector2(400, 400);
+
+        Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+        Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
+        float prevMagnitude = (touch0PrevPos - touch1PrevPos).magnitude;
+        float currentMagnitude = (touch0.position - touch1.position).magnitude;
+        float difference = currentMagnitude - prevMagnitude;
+
+
+        transform.localScale += new Vector3(_zoomSpeed * difference, _zoomSpeed * difference, 0);
+        transform.localScale = new Vector3(Mathf.Clamp(transform.localScale.x, oldTransformScale.x, oldTransformScale.x * _maxScaleZoom), Mathf.Clamp(transform.localScale.y, oldTransformScale.y, oldTransformScale.y * _maxScaleZoom));
+    }
+
+    
 }
